880df2bad14cb592565bc3cb072f59d261cc11a1
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-07-28 20:48:50 +0200

    Added favicon, parts of form, different script for handling form subit

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP test</title>
+    <title>Contact form</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
 </head>
 <body>
+
+<form action="contact_form.php">
+
+<label for="Name">Name:</label> <br>
+<input type="text" id="Name" name="Name"> <br>
+
+<label for="Email">Email address:</label> <br>
+<input type="text" name="Email" id="Email"> <br>
+
+<label for="Updates">I would like to receive updates and new related to my PHP skills.</label> <br>
+<input type="checkbox" name="Updates" id="Updates"><br>
+
+
+<input type="submit" value="Send">
+
+</form>
+
+
+
 
 <?php
 
