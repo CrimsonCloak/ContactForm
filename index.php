00eb5c39ccad634a4e8ecdbf4abc4e87bfa2ef4b
CrimsonCloak <alexander.veldeman.999@gmail.com>
2023-07-29 10:09:18 +0200

    Made processing of contact form happen and give feedback on same page

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 
-<form action="contact_form.php" method="post">
+<form action="#" method="post">
 
 <label for="Name">Name:</label> <br>
 <input type="text" id="Name" name="Name"> <br>
@@ -24,10 +24,7 @@
 
 <div id="Feedback"></div>
 <?php
-
-
-
-
+require 'contact_form.php';
 ?>
 
 
